ba77eb3a56ad77b9e21cc181e6c564475472f3d6
jitendravyas762 <jitendravyas762@gmail.com>
2023-01-16 15:15:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+// document.querySelector("button").onclick = () => {
+//     alert("hello js")
+// }
+// let b=3;
+// if(b===2){
+//     console.log("mahi")
+
+// }
+// else{
+//     console.log("me")
+// }
+// let a=465;
+// if(a%2===0){
+//     console.log("jeetu")
+// }
+// else{
+//     console.log("mahi")
+// }
+// let c=368;
+// if(c===0){
+//     if(c%2===0){
+//         console.log("nikky")
+//     }
+//     else{
+//         console.log("mahi")
+//     }
+// }
+// else{
+//     if(c%2===1){
+//         console.log("jeetu")
+//     }
+//     else{
+//         console.log("golu le papa")
+//     }
+// }
+// let jeetu=560;
+// switch(jeetu){
+//     case 1 : console.log("j");break;
+//     case 10 : console.log("k");break;
+//     case 200 : console.log("l");break;
+//     case 300 : console.log("m");break;
+//     case 540 : console.log("n");break;
+//     case 567 : console.log("0");break;
+//   default : console.log("Error")
+// }
+let jeetu = 4;
+if (jeetu === 1) {
+    console.log('monday');
+}
+    else if (jeetu === 2) {
+        console.log("tue");
+    }
+    else if (jeetu === 3) {
+        console.log("wed");
+    }
+    else if (jeetu === 0) {
+        console.log("thu");
+    }
+    else if (jeetu === 5) {
+        console.log("sat");
+    }
+    else if (jeetu === 6) {
+        console.log("fri");
+    }
+    else{
+        console.log('sunday')
+    }
---
+++ loop.js
@@ -0,0 +1,115 @@
+// for(i=1;i<=10;i++){
+//     console.log(i);
+
+// }
+
+for(i = 1; i <= 10; i++)
+{
+    for(j = 1; j <= i; j++)
+    {
+    document.write("*");
+    }
+    document.write("<br>")
+}
+
+
+
+
+for(i = 1; i <= 10; i++)
+{
+    for(j = 1; j <= i; j++)
+    {
+    document.write(j);
+    }
+    document.write("<br>")
+}
+
+
+
+
+for(i =1; i <= 5; i++)
+{
+    for(j = 5; j >=i; j--)
+    {
+    document.write(j);
+    }
+    document.write("<br>")
+}
+
+
+
+for(i =1; i <= 5; i++)
+{
+    for(j = 5; j >=i; j--)
+    {
+    document.write(j);
+    }
+    document.write("<br>")
+}
+
+
+
+for(i =5; i >= 1; i--)
+{
+    for(j = 1; j <=i; j++)
+    {
+    document.write(j);
+    }
+    document.write("<br>")
+}
+   
+
+
+
+
+
+
+
+
+
+for(i =1; i <= 5; i++)
+{
+    for(j = 1; j <=i; j++)
+    {
+    if(j%2==0){
+        document.write("#")
+    }
+    else{
+        document.write("@")
+    }
+    }
+    document.write("<br>")
+}
+
+
+
+
+
+for(i = 5; i >= 5; i--)
+{
+    for(j = 5; j >= i; j--)
+    {
+    document.write(j);
+    }
+    document.write("<br>")
+}
+for(i=5; i>=1 ; i--)
+{
+    for(j=i; j>=1; j--)
+    document.write(j)
+    document.write("<br>")
+}
+   
+
+
+
+
+
+
+
+
+    
+    
+    
+
+
